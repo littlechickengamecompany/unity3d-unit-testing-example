8a201962fe2edc88444cfba5e6dcd6599c3cef82
Eamon Woortman <eamon.woortman@gmail.com>
2016-04-06 16:47:15 +0200

    Created an interface for the turret so it's testable

--- Assets/Scripts/Game/Signals.cs
+++ Assets/Scripts/Game/Signals.cs
@@ -2,6 +2,6 @@
 using System.Collections;
 using strange.extensions.signal.impl;
 
-public class TurretClickedSignal : Signal<TurretView>{ }
+public class TurretClickedSignal : Signal<ITurret>{ }
 public class SpawnEnemySignal : Signal<EnemySpawnerView> { }
 public class BulletHitSignal : Signal<BulletView, EnemyView> { }
--- Assets/Scripts/Game/Views/TurretView.cs
+++ Assets/Scripts/Game/Views/TurretView.cs
@@ -3,10 +3,18 @@ using System.Collections;
 using strange.extensions.mediation.impl;
 using strange.extensions.signal.impl;
 using DG.Tweening;
+using System;
 
-public class TurretView : View {
+public class TurretView : View, ITurret {
 
     public readonly Signal TurretClickedSignal = new Signal();
+
+    public void Fire() {
+        BulletView bullet = GameObject.Instantiate<BulletView>(Resources.Load<BulletView>("Bullet"));
+        bullet.transform.SetParent(transform);
+        bullet.transform.position = transform.position;
+        bullet.Velocity = transform.forward * 7;
+    }
 
     private void OnMouseDown() {
         transform.DOScaleY(0.8f, 0.05f).SetLoops(2, LoopType.Yoyo);
