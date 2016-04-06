4abf8aa653c1975a7d951996adb23dbefce8127c
Eamon Woortman <eamon.woortman@gmail.com>
2016-04-06 13:47:24 +0200

    Added amazing enemy animation

--- Assets/Scripts/Game/Views/EnemyView.cs
+++ Assets/Scripts/Game/Views/EnemyView.cs
@@ -1,12 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 using strange.extensions.mediation.impl;
+using DG.Tweening;
 
 public class EnemyView : View {
 
     public Vector3 Velocity { get; internal set; }
 
-	private void Update () {
+    protected override void Start() {
+        base.Start();
+
+        transform.DOScaleZ(1.8f, 0.2f).SetLoops(40, LoopType.Yoyo);
+        Destroy(gameObject, 9);
+    }
+
+    private void Update () {
         transform.localPosition += Velocity * Time.deltaTime;
-	}
+    }
 }
