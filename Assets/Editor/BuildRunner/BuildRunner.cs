5d102968203c36bb73d2c374d2c307f93821eccd
Eamon Woortman <eamon.woortman@gmail.com>
2016-06-29 14:45:28 +0200

    Added build runner for testing a Teamcity NUnit configuration based on this post: http://astrangergravity.com/hacking-unity-from-the-command-line/

---
+++ Assets/Editor/BuildRunner/BuildRunner.cs
@@ -0,0 +1,13 @@
+﻿using SyntaxTree.VisualStudio.Unity.Bridge;
+
+namespace LCGC.BuildRunner
+{
+    public static class BuildRunner
+    {
+        public static void BuildProject()
+        {
+            // generate the project and copy the UnityAssemblies for NUnit 
+            ProjectFilesGenerator.GenerateProject();
+        }
+    }
+}
